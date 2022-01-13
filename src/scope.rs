65af05064ad3792aada5145f3ae46e7cec6a36d7
picobyte <R.oel.K.luin@gmail.com>
2022-01-13 16:58:45 +0100

    try_store_mark does not have to be recursive
    Signed-off-by: picobyte <R.oel.K.luin@gmail.com>

--- src/scope.rs
+++ src/scope.rs
@@ -24,37 +24,33 @@ pub trait WritingScope: Scope {
     fn is_repetitive(&self) -> bool;
     fn set_period(&mut self, period: Position);
     fn unset_period(&mut self);
+
+    //FIXME remove recursiveness and mutability, just return what needs to be extended & stored.
     fn try_store_mark(
         &mut self,
         ks: &mut KmerStore,
         min_idx: usize,
         min_p: ExtPosEtc,
-    ) -> Result<bool> {
+    ) -> Result<Option<(usize, ExtPosEtc)>> {
         if self.is_repetitive() && ks.kmp[min_idx].is_set() {
             ks.kmp[min_idx].set_repetitive();
-            return Ok(true);
+            return Ok(None);
         }
         let old_stored_p = ks.kmp[min_idx];
 
         if ks.kmp[min_idx].is_zero() {
             ks.set_kmp(min_idx, min_p);
-            return Ok(true);
+            return Ok(None);
         }
         if old_stored_p.is_replaceable_by(min_p) {
             if old_stored_p.is_set_and_not(min_p) {
                 // collision with a different baseindex, it had lesser extension.
-                ks.set_kmp(min_idx, min_p);
                 dbg_print!("[{:x}] -> {:?} (?)", min_idx, old_stored_p);
-                while let Some((store_idx, store_p)) =
-                    self.get_kc().get_next_xmer(min_idx, old_stored_p)
-                {
-                    if self.try_store_mark(ks, store_idx, store_p)? {
-                        break;
-                    }
-                }
+                ks.set_kmp(min_idx, min_p);
+                return Ok(Some((min_idx, old_stored_p)));
             }
             // .. else set and already min_p. Then leave bit states.
-            return Ok(true);
+            return Ok(None);
         }
         if old_stored_p.extension() == min_p.extension() {
             // If a kmer occurs multiple times within an extending readlength (repetition),
@@ -63,26 +59,35 @@ pub trait WritingScope: Scope {
                 self.set_period(dist);
                 // TODO: repetitive should be moved to higher extensions.
                 ks.kmp[min_idx].set_repetitive();
-                return Ok(true);
+                return Ok(None);
             }
             ks.kmp[min_idx].mark_more_recurs_upseq();
         }
         // collision with a different baseindex, it had greater extension.
         // the current baseindex will be extended and tried again.
         dbg_print!("not replacable, extend..");
-        Ok(false)
+        Ok(Some((min_idx, min_p)))
     }
 
     fn store_mark(&mut self, ks: &mut KmerStore, i: usize) -> Result<()> {
-        let mut min_idx = 0;
-        let mut min_p = ExtPosEtc::zero();
-        for x in 0..self.get_kc().get_ext_max() {
-            (min_idx, min_p) = self.get_d(i).get_hash_and_p(x);
-            if self.try_store_mark(ks, min_idx, min_p)? {
+        let (mut min_idx, mut min_p) = self.get_d(i).get_hash_and_p(0);
+        self.set_mark(min_idx, min_p);
+        let orig_pos = min_p.pos();
+        while let Some(next) = self.try_store_mark(ks, min_idx, min_p)? {
+            if next.1.pos() == orig_pos {
+                (min_idx, min_p) = self.get_d(i).get_hash_and_p(min_p.x() + 1);
+                self.set_mark(min_idx, min_p);
+            } else if let Some((past_idx, past_p)) = self.get_kc().get_next_xmer(next.0, next.1) {
+                // extending some pase baseidx. TODO: if frequently the same recurs,
+                // it might be worthwhile to store the reverse complement in a temp
+                min_idx = past_idx;
+                min_p = past_p;
+                // we should not store a past index in self.mark.p !!
+            } else {
+                dbg_print!("couldn't extend: [{}] {:?} ..?", next.0, next.1);
                 break;
             }
         }
-        self.set_mark(min_idx, min_p);
         Ok(())
     }
 }
